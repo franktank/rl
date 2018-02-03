d8c4eab5f2fe8db4ad962ca1d2f4b78d370018b8
Franky Liang <fliangz96@gmail.com>
2018-02-03 14:43:47 -0500

    use keras for model in policy gradients

--- pong-with-denis.py
+++ pong-with-denis.py
@@ -1,31 +1,26 @@
 """ Trains an agent with (stochastic) Policy Gradients on Pong. Uses OpenAI Gym. """
 import numpy as np
-import cPickle as pickle
+# import cPickle as pickle
 import gym
+import random
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 
 # hyperparameters
-H = 200 # number of hidden layer neurons
-batch_size = 10 # every how many episodes to do a param update?
+# batch_size = 10 # every how many episodes to do a param update?
 learning_rate = 1e-4
 gamma = 0.99 # discount factor for reward
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
 resume = False # resume from previous checkpoint?
-render = False
+render = True
 
-# model initialization
-D = 80 * 80 # input dimensionality: 80x80 grid
-if resume:
-  model = pickle.load(open('save.p', 'rb'))
-else:
-  model = {}
-  model['W1'] = np.random.randn(H,D) / np.sqrt(D) # "Xavier" initialization
-  model['W2'] = np.random.randn(H) / np.sqrt(H)
-
-grad_buffer = { k : np.zeros_like(v) for k,v in model.iteritems() } # update buffers that add up gradients over a batch
-rmsprop_cache = { k : np.zeros_like(v) for k,v in model.iteritems() } # rmsprop memory
-
-def sigmoid(x):
-  return 1.0 / (1.0 + np.exp(-x)) # sigmoid "squashing" function to interval [0,1]
+model = Sequential()
+model.add(Dense(200, input_dim=6400, activation='relu'))
+model.add(Dense(200, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='mse',
+              optimizer=Adam(lr=learning_rate))
 
 def prepro(I):
   """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
@@ -40,36 +35,16 @@ def discount_rewards(r):
   """ take 1D float array of rewards and compute discounted reward """
   discounted_r = np.zeros_like(r)
   running_add = 0
-  for t in reversed(xrange(0, r.size)):
+  for t in reversed(range(0, r.size)):
     if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
     running_add = running_add * gamma + r[t]
     discounted_r[t] = running_add
   return discounted_r
 
-def policy_forward(x):
-  h = np.dot(model['W1'], x)
-  h[h<0] = 0 # ReLU nonlinearity
-  logp = np.dot(model['W2'], h)
-  p = sigmoid(logp)
-  """
-  p is a number because of shape of w2
-  """
-  return p, h # return probability of taking action 2, and hidden state
-
-def policy_backward(eph, epdlogp):
-  "just backprop"
-  """ backward pass. (eph is array of intermediate hidden states) """
-  dW2 = np.dot(eph.T, epdlogp).ravel()
-  # outer product
-  dh = np.outer(epdlogp, model['W2'])
-  dh[eph <= 0] = 0 # backpro prelu
-  dW1 = np.dot(dh.T, epx)
-  return {'W1':dW1, 'W2':dW2}
-
 env = gym.make("Pong-v0")
 observation = env.reset()
 prev_x = None # used in computing the difference frame
-xs,hs,dlogps,drs = [],[],[],[]
+xs,dlogps,drs = [],[],[]
 running_reward = None
 reward_sum = 0
 episode_number = 0
@@ -78,11 +53,12 @@ while True:
 
   # preprocess the observation, set input to network to be difference image
   cur_x = prepro(observation)
-  x = cur_x - prev_x if prev_x is not None else np.zeros(D)
+  x = cur_x - prev_x if prev_x is not None else np.zeros(6400)
   prev_x = cur_x
 
   # forward the policy network and sample an action from the returned probability
-  aprob, h = policy_forward(x)
+
+  aprob = model.predict(np.reshape(x, [1,6400]))
   action = 2 if np.random.uniform() < aprob else 3 # roll the dice!
   """
   pong conventions
@@ -90,7 +66,6 @@ while True:
 
   # record various intermediates (needed later for backprop)
   xs.append(x) # observation
-  hs.append(h) # hidden state
   y = 1 if action == 2 else 0 # a "fake label"
 
   """
@@ -109,7 +84,6 @@ while True:
 
     # stack together all inputs, hidden states, action gradients, and rewards for this episode
     epx = np.vstack(xs)
-    eph = np.vstack(hs)
     epdlogp = np.vstack(dlogps)
     epr = np.vstack(drs)
     xs,hs,dlogps,drs = [],[],[],[] # reset array memory
@@ -123,25 +97,17 @@ while True:
     discounted_epr /= np.std(discounted_epr)
 
     epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.)
-    "backprop"
-    grad = policy_backward(eph, epdlogp)
-    for k in model: grad_buffer[k] += grad[k] # accumulate grad over batch
+    "train"
+    model.fit(epx, epdlogp, epochs=1, verbose=0)
 
-    # perform rmsprop parameter update every batch_size episodes
-    if episode_number % batch_size == 0:
-      for k,v in model.iteritems():
-        g = grad_buffer[k] # gradient
-        rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2
-        model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
-        grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
 
     # boring book-keeping
     running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-    print 'resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward)
-    if episode_number % 100 == 0: pickle.dump(model, open('save.p', 'wb'))
+    print ('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
+    # if episode_number % 100 == 0: pickle.dump(model, open('save.p', 'wb'))
     reward_sum = 0
     observation = env.reset() # reset env
     prev_x = None
 
   if reward != 0: # Pong has either +1 or -1 reward exactly when game ends.
-    print ('ep %d: game finished, reward: %f' % (episode_number, reward)) + ('' if reward == -1 else ' !!!!!!!!')
+    print ('ep %d: game finished, reward: %f' %(episode_number, reward)) #+ ('' if reward == -1 else ' !!!!!!!!')
